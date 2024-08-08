840bc435ab33e2da4a0c7228b1a49d1c97ea1b25
AyushMali-17 <ayushmali1708@gmail.com>
2024-08-08 16:18:39 +0000

    just started -2

--- script.js
+++ script.js
@@ -16,13 +16,40 @@ function updateClock(timezone, hourHand, minuteHand, secondHand) {
 }
 
 function startClocks() {
-    const hour1 = document.getElementById('hour1');
-    const minute1 = document.getElementById('minute1');
-    const second1 = document.getElementById('second1');
+    const clocks = [
+        { timezone: 'America/New_York', hour: 'hour1', minute: 'minute1', second: 'second1' },
+        { timezone: 'Europe/London', hour: 'hour2', minute: 'minute2', second: 'second2' },
+        { timezone: 'Asia/Tokyo', hour: 'hour3', minute: 'minute3', second: 'second3' },
+        { timezone: 'Australia/Sydney', hour: 'hour4', minute: 'minute4', second: 'second4' },
+        { timezone: 'Europe/Moscow', hour: 'hour5', minute: 'minute5', second: 'second5' }
+    ];
 
     setInterval(() => {
-        updateClock('America/New_York', hour1, minute1, second1);
+        clocks.forEach(clock => {
+            const hourHand = document.getElementById(clock.hour);
+            const minuteHand = document.getElementById(clock.minute);
+            const secondHand = document.getElementById(clock.second);
+            updateClock(clock.timezone, hourHand, minuteHand, secondHand);
+        });
     }, 1000);
+}
+
+function changeColors() {
+    const faces = document.querySelectorAll('.face');
+    faces.forEach(face => {
+        face.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    });
+}
+
+function toggleRotation() {
+    const clocks = document.querySelectorAll('.clock');
+    clocks.forEach(clock => {
+        if (clock.style.animationPlayState === 'paused') {
+            clock.style.animationPlayState = 'running';
+        } else {
+            clock.style.animationPlayState = 'paused';
+        }
+    });
 }
 
 startClocks();
